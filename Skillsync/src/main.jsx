6ed19295af6c1cc074969ad8d1bf71589c607d8f
fakihazahoor168-ctrl <fakihazahoor168@gmail.com>
2026-06-16 21:19:47 +0500

    Configure dynamic API URL using axios interceptor and VITE_API_URL

--- Skillsync/src/main.jsx
+++ Skillsync/src/main.jsx
@@ -8,6 +8,18 @@ import "./styles/dashboard.css";
 import "./styles/buttons.css";
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+// Redirect localhost backend requests to the deployed Render backend URL when configured
+axios.interceptors.request.use((config) => {
+  const apiBase = import.meta.env.VITE_API_URL;
+  if (apiBase && config.url && config.url.startsWith('http://localhost:5000')) {
+    config.url = config.url.replace('http://localhost:5000', apiBase);
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
